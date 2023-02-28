78bb4b96f0b2131c0fa67ed643a4bb3f0602a3ba
ElDav1d <dvivoes@yahoo.es>
2023-02-28 17:51:12 +0100

    Resolves Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import data from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const initialContacts = data.slice(0, 5);
+  const [contacts, setContacts] = useState(initialContacts);
+
+  console.log(contacts);
+
   return (
     <div className="App">
       <h1>hello world</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {contacts.map((contact) => (
+          <tr>
+            <td>
+              <img
+                src={contact.pictureUrl}
+                alt={`a picture of ${contact.name}`}
+              />
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
